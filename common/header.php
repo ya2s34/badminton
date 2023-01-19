f7d17cda57d0dac0789437574465cc383e989fa4
ya2s34 <115533136+ya2s34@users.noreply.github.com>
2023-01-19 09:41:05 +0100

    Add files via upload

---
+++ common/header.php
@@ -0,0 +1,7 @@
+
+    <header>
+        <h1 class="site-heading text-center text-faded d-none d-lg-block">
+            <span class="site-heading-upper text-primary mb-3">TP intégration en php</span>
+            <span class="site-heading-lower">Greta badminton</span>
+        </h1>
+    </header>
